1acd991bd42ba453878aed34d472ad3579635156
Mitar <mitar.git@tnode.com>
2015-02-14 22:23:55 -0800

    Updated to new versions and added Meteor.

--- todomvc/react/js/todoItem.jsx
+++ todomvc/react/js/todoItem.jsx
@@ -1,19 +1,18 @@
-/**
- * @jsx React.DOM
- */
 /*jshint quotmark: false */
 /*jshint white: false */
 /*jshint trailing: false */
 /*jshint newcap: false */
-/*global React, Utils */
-(function (window) {
+/*global React */
+var app = app || {};
+
+(function () {
 	'use strict';
 
 	var ESCAPE_KEY = 27;
 	var ENTER_KEY = 13;
 
-	window.TodoItem = React.createClass({
-		handleSubmit: function () {
+	app.TodoItem = React.createClass({
+		handleSubmit: function (event) {
 			var val = this.state.editText.trim();
 			if (val) {
 				this.props.onSave(val);
@@ -21,14 +20,13 @@
 			} else {
 				this.props.onDestroy();
 			}
-			return false;
 		},
 
 		handleEdit: function () {
 			// react optimizes renders by batching them. This means you can't call
 			// parent's `onEdit` (which in this case triggeres a re-render), and
 			// immediately manipulate the DOM as if the rendering's over. Put it as a
-			// callback. Refer to app.js' `edit` method
+			// callback. Refer to app.jsx' `edit` method
 			this.props.onEdit(function () {
 				var node = this.refs.editField.getDOMNode();
 				node.focus();
@@ -38,11 +36,11 @@
 		},
 
 		handleKeyDown: function (event) {
-			if (event.keyCode === ESCAPE_KEY) {
+			if (event.which === ESCAPE_KEY) {
 				this.setState({editText: this.props.todo.title});
-				this.props.onCancel();
-			} else if (event.keyCode === ENTER_KEY) {
-				this.handleSubmit();
+				this.props.onCancel(event);
+			} else if (event.which === ENTER_KEY) {
+				this.handleSubmit(event);
 			}
 		},
 
@@ -55,14 +53,14 @@
 		},
 
 		/**
-		 * This is a completely optional performance enhancement that you can implement
-		 * on any React component. If you were to delete this method the app would still
-		 * work correctly (and still be very performant!), we just use it as an example
-		 * of how little code it takes to get an order of magnitude performance improvement.
+		 * This is a completely optional performance enhancement that you can
+		 * implement on any React component. If you were to delete this method
+		 * the app would still work correctly (and still be very performant!), we
+		 * just use it as an example of how little code it takes to get an order
+		 * of magnitude performance improvement.
 		 */
 		shouldComponentUpdate: function (nextProps, nextState) {
 			return (
-				nextProps.todo.id !== this.props.todo.id ||
 				nextProps.todo !== this.props.todo ||
 				nextProps.editing !== this.props.editing ||
 				nextState.editText !== this.state.editText
@@ -79,7 +77,7 @@
 						<input
 							className="toggle"
 							type="checkbox"
-							checked={this.props.todo.completed ? 'checked' : null}
+							checked={this.props.todo.completed}
 							onChange={this.props.onToggle}
 						/>
 						<label onDoubleClick={this.handleEdit}>
@@ -99,4 +97,4 @@
 			);
 		}
 	});
-})(window);
+})();
